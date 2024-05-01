18fbd8822b06fb910ee23e119d1877927ebff77b
james nyakundi <omarejames082@gmail.com>
2024-05-01 16:54:02 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -18,11 +18,19 @@ function Home() {
 
 {/* write an <About> component here */}
 
-
+function About() {
+  return (
+    <div id="about">
+      <h1>Home</h1>
+    </div>
+  );
+}
 function App() {
   return (
     <div>
       <NavBar />
+      <Home/>
+      <About/>
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
